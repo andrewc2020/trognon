de46c1c2b479c18be90b1f0d6c606d02da0afcc6
andrewc2020 <andrewc2020@gmail.com>
2020-09-16 22:49:32 +0100

    added typescript eslint prettier

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports ={
+    semi:false,
+    trailingComma:'es6',
+    singleQuote:true,
+    printWidth:100,
+    tabWidth:2,
+    useTabs:false
+}
---
+++ pages/index.tsx
@@ -0,0 +1,30 @@
+
+import Head from 'next/head'
+
+import React from 'react';
+import { GetServerSideProps } from 'next';
+
+
+
+type Props ={
+  name: string
+}
+
+const Home : React.FunctionComponent<Props> = ({name}) =>(
+  <div>
+    <p>
+      HELLO {name}!
+    </p>
+  </div>
+)
+
+export default Home
+
+export const getServerSideProps : GetServerSideProps<Props>= async ({query}:any) => {
+   const name = query.name instanceof Array?query.name.join(',  '):query.name
+  return {
+    props:{
+      name:query.name || "World",
+    }
+  }
+}
